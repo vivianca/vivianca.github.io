18577fecff5852ea1eaf44e08babfabd65caa8f5
Vivian <vivianca@stanford.edu>
2018-06-05 12:06:54 -0700

    -

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,6 +14,8 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
+
+method="post"
 	
 // Create the email and send the message
 $to = 'firstgirlpianist@live.ca'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
