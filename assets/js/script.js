4eb8cc5f3431a27ce0e9918db62f707e097ba97e
vivianca <vivianca@stanford.edu>
2023-10-07 22:32:13 -0700

    rounding icon and deleting some unnecessary code

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,6 @@ sidebarBtn.addEventListener("click", function () {
   elementToggleFunc(sidebar);
 });
 
-// testimonials variables
-// const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
-// const modalContainer = document.querySelector("[data-modal-container]");
-// const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-// const overlay = document.querySelector("[data-overlay]");
-
 // modal variable
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
@@ -30,46 +24,11 @@ const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 };
-
-// add click event to all modal items
-// for (let i = 0; i < testimonialsItem.length; i++) {
-//   testimonialsItem[i].addEventListener("click", function () {
-//     modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-//     modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-//     modalTitle.innerHTML = this.querySelector(
-//       "[data-testimonials-title]"
-//     ).innerHTML;
-//     modalText.innerHTML = this.querySelector(
-//       "[data-testimonials-text]"
-//     ).innerHTML;
-
-//     testimonialsModalFunc();
-//   });
-// }
-
-// add click event to modal close button
-// modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-// overlay.addEventListener("click", testimonialsModalFunc);
-
 // custom select variables
 // const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
-
-// select.addEventListener("click", function () { elementToggleFunc(this); });
-
-// // add event in all select items
-// for (let i = 0; i < selectItems.length; i++) {
-//   selectItems[i].addEventListener("click", function () {
-
-//     let selectedValue = this.innerText.toLowerCase();
-//     selectValue.innerText = this.innerText;
-//     elementToggleFunc(select);
-//     filterFunc(selectedValue);
-
-//   });
-// }
 
 // filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
